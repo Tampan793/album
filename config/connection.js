4eac23607f70d50ae7e31d0905ab31a0e20fd8ee
Tampan793 <69372834+Tampan793@users.noreply.github.com>
2021-11-26 20:13:08 +0700

    Add files via upload

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient
+const collection = require('./collection')
+const config = require('../config.js')
+require('dotenv').config()
+
+const state = {
+    db: null
+}
+
+module.exports.connect = function (done) {
+        const url = config.DB_URL
+        const dbname = 'MediaFile'
+
+    MongoClient.connect(url,{ useUnifiedTopology: true } , (err, data) => {
+        if (err) return done(err)
+        state.db = data.db(dbname)
+        done()
+    })
+
+    
+}
+
+module.exports.get = function () {
+    return state.db
+}
