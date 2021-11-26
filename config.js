c2cdd24884108ee2b83528160215c9ed72fc526d
Tampan793 <69372834+Tampan793@users.noreply.github.com>
2021-11-26 20:01:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+        //Token bot disediakan oleh  @botfather
+    TOKEN: '',
+
+    //Masukkan domain / ip yang telah disetting di nginx
+    DOMAIN: '',
+
+    //Masukkan port yang telah disetting di nginx
+    PORT: '',
+
+    //Dapatkan url basis data mongodb dari https://www.mongodb.com/cloud/atlas with database name: mediaFile and collection name: fileBackup
+    DB_URL: '',
+
+        //Masukkan id akun kamu, jika ada admin ke 2 dan ke 3 isi ADMIN1 dan ADMIN2
+    ADMIN: '',
+
+        //Masukkan nama pengguna bot Anda tanpa @ dan harus huruf kecil
+    BOTUSERNAME: '',
+
+        //Buat saluran pribadi dan masukkan id saluran untuk memantau file dan mendapatkan data pengguna dan file
+    LOG_CHANNEL: '',
+
+        //Caption ketika link media yang dikirim di tekan
+    CAPTIONLINK: '',
+
+        //Masukkan pesan sebelum pengguna bergabung
+    WELCOMEJOINBOT: '',
+
+        //Masukkan pesan selamat datang ketika pengguna bergabung
+    MESSAGEWELCOMEBOT: '',
+
+        //Masukkan ID channel grup untuk identifikasi bot bisa membaca pengguna yang masuk
+    CHANNELJOIN: '',
+
+        //Link channel grup masukkan disini dan ikuti format penulisan
+    LINKCHANNEL: 'Gabung-https://t.me/test',
+}
---
+++ help.js
@@ -0,0 +1,3 @@
+module.exports.bothelp = `Halo, kami adalah bot File Saver. Kami akan terus memperbarui bot kami.`;
+
+module.exports.botsrc = `Made by @painofwords $`;
